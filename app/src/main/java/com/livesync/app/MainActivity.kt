05a30e616fde1ad04c3bd5e085486940bad3c46e
Athr894 <athr894@gmail.com>
2026-07-29 04:40:06 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/livesync/app/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.livesync.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            allowFileAccess = true
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://github.com")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
